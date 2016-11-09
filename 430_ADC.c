6a99072bf9ed046efd1568f8f012eb898187ef40
Jun Zhang <jun.zhang.2015@uni.strath.ac.uk>
2016-11-09 19:23:11 +0000

    sinusoidal PWM generation
    update 
       TBCCR1=T_1;       //s[i];
       TBCCR3=T_3;       //s[i+n];
       TBCCR5=T_5;       //s[i+N2];

--- 430_ADC.c
+++ 430_ADC.c
@@ -5,32 +5,96 @@
  **************************************************/
 #include <msp430.h>
 #include <driverlib.h>
-#include "LedDriver.h"
 #include "adc.h"
-
 
 int SW1_interruptFlag_=1;
 int SW2_interruptFlag_ ;
 float rate=1 ;
 int sample;
 
+#define  Cp 360
+/*----------系统频率--载波频率 调制波频率--最大脉宽--最小脉宽--*/
+unsigned int sysHz=10, triHz=9, sinHz=50,  HzMax=540,   HzMin=15, HzM=555,N2,k=0;
+/*--调制系数--MHz------KHz--------Hz-----------------微秒---*/
+/*----------载波比----PI/N--PI/4--PI*3/4-Tc/4-Tc*m/4-sine[j]/q14--*/
+unsigned int N_ts=180,PI_N,PI_25,PI_75,Tc_2,Tc_2m,Sine_Q14;
+float  m=0.9;
+//unsigned int s[1204];
+int sine[361]={0,286,572,857,1143,1428,           //SIN值表,Q14格式有符号数
+             1713,1997,2280,2563,2845,
+             3126,3406,3686,3964,4240,
+             4516, 4790,5063,5334,5604,
+             5872,6138,6402,6664, 6924,
+             7182,7438,7692,7943,8192,
+             8438,8682,8923,9162,9397,
+             9630,9860,10087,10311,10531,
+             10749,10963,11174,11381,11585,
+             11786,11982,12176,12365,12551,
+             12733,12911,13085,13255,13421,
+             13583,13741,13894,14044,14189,
+             14330,14466,14598,14726,14849,
+             14968,15082,15191,15296,15396,
+             15491,15582,15668,15749,15826,
+             15897,15964,16026,16083,16135,
+             16182,16225,16262,16294,16322,
+             16344,16362,16374, 16382,16384,            //;90
+             16382,16374,16362,16344,16322,
+             16294,16262,16225,16182,16135,
+             16083,16026,15964,15897,15826,
+             15749,15668,15582,15491,15396,
+             15296,15191,15082,14968,14849,
+             14726,14598,14466,14330,14189,
+             14044,13894,13741,13583,13421,
+             13255,13085,12911,12733,12551,
+             12365,12176,11982,11786,11585,
+             11381,11174,10963,10749,10531,
+             10311,10087,9860,9630,9397,
+             9162,8923,8682,8438,8192,
+             7943,7692,7438,7182,6924,
+             6664,6402,6138,5872,5604,
+             5334,5063,4790,4516,4240,
+             3964,3686,3406,3126,2845,
+             2563,2280,1997,1713,1428,
+             1143,857,572,286, 0,                      //;180
+             286,572,857,1143,1428,
+             1713,1997,2280,2563,2845,
+             3126,3406,3686,3964,4240,
+             4516, 4790,5063,5334,5604,
+             5872,6138,6402,6664, 6924,
+             7182,7438,7692,7943,8192,
+             8438,8682,8923,9162,9397,
+             9630,9860,10087,10311,10531,
+             10749,10963,11174,11381,11585,
+             11786,11982,12176,12365,12551,
+             12733,12911,13085,13255,13421,
+             13583,13741,13894,14044,14189,
+             14330,14466,14598,14726,14849,
+             14968,15082,15191,15296,15396,
+             15491,15582,15668,15749,15826,
+             15897,15964,16026,16083,16135,
+             16182,16225,16262,16294,16322,
+             16344,16362,16374, 16382,16384,            //;90
 
-#pragma vector = PORT1_VECTOR
-__interrupt void P1_ISR(void)
-{
-   switch(__even_in_range(P1IV,P1IV_P1IFG7))
-{
-     case P1IV_P1IFG3: //It is SW1
-      	SW1_interruptFlag_ = -SW1_interruptFlag_;
-    	GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN3);
-      break;
+             16382,16374,16362,16344,16322,
+             16294,16262,16225,16182,16135,
+             16083,16026,15964,15897,15826,
+             15749,15668,15582,15491,15396,
+             15296,15191,15082,14968,14849,
+             14726,14598,14466,14330,14189,
+             14044,13894,13741,13583,13421,
+             13255,13085,12911,12733,12551,
+             12365,12176,11982,11786,11585,
+             11381,11174,10963,10749,10531,
+             10311,10087,9860,9630,9397,
+             9162,8923,8682,8438,8192,
+             7943,7692,7438,7182,6924,
+             6664,6402,6138,5872,5604,
+             5334,5063,4790,4516,4240,
+             3964,3686,3406,3126,2845,
+             2563,2280,1997,1713,1428,
+             1143,857,572,286, 0};
 
-      case P1IV_P1IFG4:  //It is SW2
-    	SW2_interruptFlag_ = 1;
-    	GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN4);
-      break;
-}
-}
+
 
 // ADC interrupt service routine
 #pragma vector=ADC_VECTOR           // these two lines are used by IAR and CCC
@@ -50,9 +114,9 @@ __interrupt void ADC_ISR(void)
 
 //void PWM_setUp()
 //{
-//   P1DIR |= BIT7+BIT6;  //set pin as output 
+//   P1DIR |= BIT7+BIT6;  //set pin as output
 //   P1SEL0 |= BIT7+BIT6; //P1.7 TA0.1, P1.6 TA0.2, Timer TA0 CCR1 capture: CCI1A input, compare: Out1 outputs PWM1
-//   
+//
 ////   TA0CCR0 = 0x0800;   //2048  8hz
 ////   TA0CCR1 = 0x0700;   // duty period=1/16?
 //   TA0CCTL1= OUTMOD_3;  //TA0.1 set (TACCR1) / reset (TACCR0)
@@ -61,83 +125,175 @@ __interrupt void ADC_ISR(void)
 //   TA0CCR1= 5;   //PWM Duty Cycle
 ////   TA0CCR0= PWMprd;     //period of PWM = PWMprd/ACLK =(max/frequency)
 ////   TA0CCR1= PWMduty;    //high level output time of PWM1 = Duty Cycle * Period; duty cycle= CCR1/CCR0
-////   TA0CCR2= PWMduty;    //                          PWM2     
+////   TA0CCR2= PWMduty;    //                          PWM2
 
 ////   TA0CTL=TASSEL_2+MC_1;//SMCLK (maximum operating frequency 16MHz), up-mode
-//   TA0CTL=TASSEL_1+MC_1;//ACLK (approximately 32 kHz), Up mode: Timer counts up to TAxCCR0                     
+//   TA0CTL=TASSEL_1+MC_1;//ACLK (approximately 32 kHz), Up mode: Timer counts up to TAxCCR0
 //   TA0CTL|=ID_0;       //00b = /1, 01b = /2, 10b = /4, 11b = /8
 //
 //   TA0CTL |= TACLR;    //Timer_A clear The TACLR bit is automatically reset and always reads as zero
 //   TA0CTL |= TAIE;      //Timer_A interrupt enable.
 //   TA0CCTL0 |= CCIE;
 //   TA0CCTL1 |= CCIE;  //Capture/compare interrupt enable. This bit enables the interrupt request of the corresponding CCIFG flag.
-//
 //}
+
+void ADC_Init();
+void SwitchInit();
+void CalInit();
 
 int main(void)
 {
-   ADC_init(ADC_BASE,
-            ADC_SAMPLEHOLDSOURCE_SC,
-            ADC_CLOCKSOURCE_ADCOSC,
-            ADC_CLOCKDIVIDER_1);
-   ADC_enable(ADC_BASE);
-   ADC_enableInterrupt(ADC_BASE,ADC_COMPLETED_INTERRUPT);
-   ADC_clearInterrupt(ADC_BASE,ADC_COMPLETED_INTERRUPT);
-   ADC_setupSamplingTimer(ADC_BASE, ADC_CYCLEHOLD_4_CYCLES, ADC_MULTIPLESAMPLESDISABLE);
-   ADC_configureMemory(ADC_BASE,
-                       ADC_INPUT_A9,
-                       ADC_VREFPOS_AVCC,
-                       ADC_VREFNEG_AVSS);
-
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+  ADC_Init();
+  //PWM_setUp()
+  CalInit();
   //Default MCLK = 1MHz
 
   unsigned int i = 0;
-
   unsigned char dialValue = 0x01;
-
-  WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-
-  initialiseLedDial();
-  //PWM_setUp()
 
   // Disable the GPIO power-on default high-impedance mode
   // to activate previously configured port settings
   PMM_unlockLPM5();
 
-  //Switch 1
+  __enable_interrupt();
+
+  for (;;)
+  {
+    _BIS_SR(CPUOFF);                    // CPU off
+    _NOP();                             // Required only for C-spy
+  }
+
+  while(1)
+  {
+    ADC_startConversion(ADC_BASE, ADC_REPEATED_SINGLECHANNEL);
+
+    rate=0.5 + sample/256;  //sample between 0~1023
+    //Refresh display (10 times for each position)
+    for(int i = 0; i < 10*rate; i++)      //100ms
+      refreshLedDial();
+  }
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void P1_ISR(void)
+{
+   switch(__even_in_range(P1IV,P1IV_P1IFG7))
+{
+     case P1IV_P1IFG3: //It is SW1
+      	SW1_interruptFlag_ = -SW1_interruptFlag_;
+    	GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN3);
+      break;
+
+      case P1IV_P1IFG4:  //It is SW2
+    	SW2_interruptFlag_ = 1;
+    	GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN4);
+      break;
+}
+}
+// -------------TimerB interrupt service routine-----------
+#pragma vector=TIMERB1_VECTOR
+__interrupt void Timer_B(void)
+{
+   TBCTL&=~TBIFG;
+   unsigned int j;
+   int T_1,T_3,T_5;
+   unsigned long Tc2mSQ;
+   //-------------------------T_1--------------------------
+   j=k*PI_N;
+   if((j<360)&&(j>=180))
+   {
+     Tc2mSQ=((long)Tc_2m*sine[j]);
+     Tc2mSQ=Tc2mSQ>>14;
+     T_1=Tc_2-Tc2mSQ;
+   }
+   else
+   {
+     if(j>=Cp)
+       j=j-Cp;
+     Tc2mSQ=((long)Tc_2m*sine[j]);
+     Tc2mSQ=Tc2mSQ>>14;
+     T_1=Tc_2+Tc2mSQ;
+   }
+   //-------------------------T_2--------------------------
+   j=j+120;
+   if((j<360)&&(j>=180))
+   {
+     Tc2mSQ=((long)Tc_2m*sine[j]);
+     Tc2mSQ=Tc2mSQ>>14;
+     T_3=Tc_2-Tc2mSQ;
+   }
+   else
+   {
+     if(j>=Cp)
+       j=j-Cp;
+     Tc2mSQ=((long)Tc_2m*sine[j]);
+     Tc2mSQ=Tc2mSQ>>14;
+     T_3=Tc_2+Tc2mSQ;
+   }
+   //-------------------------T_3--------------------------
+   j=j+120;
+   if((j<360)&&(j>=180))
+   {
+     Tc2mSQ=((long)Tc_2m*sine[j]);
+     Tc2mSQ=Tc2mSQ>>14;
+     T_5=Tc_2-Tc2mSQ;
+   }
+   else
+   {
+     if(j>=Cp)
+       j=j-Cp;
+     Tc2mSQ=((long)Tc_2m*sine[j]);
+     Tc2mSQ=Tc2mSQ>>14;
+     T_5=Tc_2+Tc2mSQ;
+   }
+   if(T_1<HzMin)
+     T_1=0;
+   if(T_1>HzMax)
+     T_1=HzM;
+   if(T_3<HzMin)
+     T_3=0;
+   if(T_3>HzMax)
+     T_3=HzM;
+   if(T_5<HzMin)
+     T_5=0;
+   if(T_5>HzMax)
+     T_5=HzM;
+   TBCCR1=T_1;       //s[i];
+   TBCCR3=T_3;       //s[i+n];
+   TBCCR5=T_5;       //s[i+N2];
+   k++;
+   if(k>N2)
+     k=0;
+//   TBCTL |= TBCLR;
+//   TBCTLTBSSEL_2+TBIE;
+}
+
+void ADC_Init(void)
+{
+  ADC_init(ADC_BASE,
+           ADC_SAMPLEHOLDSOURCE_SC,
+           ADC_CLOCKSOURCE_ADCOSC,
+           ADC_CLOCKDIVIDER_1);
+  ADC_enable(ADC_BASE);
+  ADC_enableInterrupt(ADC_BASE,ADC_COMPLETED_INTERRUPT);
+  ADC_clearInterrupt(ADC_BASE,ADC_COMPLETED_INTERRUPT);
+  ADC_setupSamplingTimer(ADC_BASE, ADC_CYCLEHOLD_4_CYCLES, ADC_MULTIPLESAMPLESDISABLE);
+  ADC_configureMemory(ADC_BASE,
+                      ADC_INPUT_A9,
+                      ADC_VREFPOS_AVCC,
+                      ADC_VREFNEG_AVSS);
+
+  //ADC P8.1 A9
+  GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P8, GPIO_PIN1, GPIO_PRIMARY_MODULE_FUNCTION);
+}
+
+void SwitchInit(void)
+{
    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN3);
    GPIO_selectInterruptEdge(GPIO_PORT_P1, GPIO_PIN3, GPIO_LOW_TO_HIGH_TRANSITION);
    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN3);
 
    GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN3);
    GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN3);
-
-  __enable_interrupt();
-
-  //ADC P8.1 A9
-  GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P8, GPIO_PIN1, GPIO_PRIMARY_MODULE_FUNCTION);
-
-  while(1)
-  {
-    ADC_startConversion(ADC_BASE, ADC_REPEATED_SINGLECHANNEL);
-
-    if (SW1_interruptFlag_==1)
-    {
- 
-    }
-    //if(SW1_interruptFlag_==-1)
-    else
-    {
-
-    }
-
-    rate=0.5 + sample/256;
-
-    //Set value
-    setLedDial(dialValue);
-
-    //Refresh display (10 times for each position)
-    for(int i = 0; i < 10*rate; i++)      //100ms
-      refreshLedDial();
-  }
 }
